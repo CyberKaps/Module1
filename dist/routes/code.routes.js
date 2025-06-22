98631777c61cd9886cf90c680af86f3aa20e6d4b
Kalpesh Borde <bordekalpesh@gmail.com>
2025-06-22 18:49:02 +0530

    change

--- dist/routes/code.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const code_controller_1 = require("../controllers/code.controller");
-const router = (0, express_1.Router)();
-router.post('/analyze', (req, res, next) => {
-    Promise.resolve((0, code_controller_1.analyzeCodeController)(req, res, next)).catch(next);
-});
-exports.default = router;
